950e3ae356a1978568fb2c89c804a4b3837ae7a5
Chris <5125@holbertonschool.com>
2022-08-10 14:57:25 +0200

    ac error fixed

--- _getenv.c
+++ _getenv.c
@@ -36,14 +36,3 @@ char *_getenv(const char *name)
 
 	return (NULL);
 }
-
-int main(int ac, char **av)
-{
-
-	if (_getenv(av[1]) != NULL)
-		printf("The environement variable for %s is %s\n", av[1], _getenv(av[1]));
-	else
-		printf("There is no environement variable for %s", av[1]);
-
-	return (0);
-}
